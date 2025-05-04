ed7c7fb0e48cff6be6d45714896a2b05f0fe64c6
Oleh Proidakov <first.god.hermit@gmail.com>
2025-05-05 00:29:11 +0300

    v4.3.0/fix-workflow

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: process.env.PAGES_BASE_PATH,
   images: {
     loader: 'custom',
     loaderFile: './src/app/image.ts',
